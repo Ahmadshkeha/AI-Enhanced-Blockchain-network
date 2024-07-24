ce6c5b19188d09400eb522553df7f6d1a176aff0
ahmad <ahmad.shekha26@gmail.com>
2024-07-24 20:26:08 +0300

    conflict commit

--- wallet/index.js
+++ wallet/index.js
@@ -1,4 +1,4 @@
-const Transaction = require('./transaction');
+//const Transaction = require('./transaction');
 const { STARTING_BALANCE } = require('../config');
 const { ec, cryptoHash } = require('../util');
 
