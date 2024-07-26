9ff540a2e2920eee3c646e99fc7f734180b3423d
ahmad <ahmad.shekha26@gmail.com>
2024-07-26 14:11:50 +0300

    first commit for main branch

--- blockchain/block.js
+++ blockchain/block.js
@@ -3,7 +3,7 @@ const { GENESIS_DATA, MINE_RATE } = require('../config');
 const { cryptoHash } = require('../util');
 const MerkleTree = require('../util/merkleTree');
 const { json } = require('body-parser');
-
+console.log("This is update for main branch");
 class Block {
   constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
